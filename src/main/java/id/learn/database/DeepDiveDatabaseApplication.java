a8b1c313c6f0530179c8d5d72a97d9217b6fad79
Harist Islami <islamiharist1998@gmail.com>
2023-04-18 12:51:47 +0700

    Initial Commit

---
+++ src/main/java/id/learn/database/DeepDiveDatabaseApplication.java
@@ -0,0 +1,13 @@
+package id.learn.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDiveDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepDiveDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/learn/database/DeepDiveDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package id.learn.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDiveDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
