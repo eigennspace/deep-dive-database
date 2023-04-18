b04dffce30900ce80d753cb944eae40c6dc6846a
Harist Islami <islamiharist1998@gmail.com>
2023-04-18 13:32:55 +0700

    + unit test bookrepository

--- src/test/java/id/learn/database/DeepDiveDatabaseApplicationTests.java
+++ src/test/java/id/learn/database/DeepDiveDatabaseApplicationTests.java
@@ -1,13 +1,22 @@
 package id.learn.database;
 
+import id.learn.database.jpa.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class DeepDiveDatabaseApplicationTests {
 
+    @Autowired
+    BookRepository bookRepository;
     @Test
-    void contextLoads() {
+    void testBookRepository() {
+        long count = bookRepository.count();
+
+        assertThat(count).isGreaterThan(0);
     }
 
 }
